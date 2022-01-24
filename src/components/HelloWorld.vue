f63869cd91f501a8238e9225b1fc4127eaed8cf9
Okarin1 <Okarin1024@gmail.com>
2022-01-25 00:10:49 +0800

    增加侧边栏

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="okarin">
-    <poem/>
+    <poem />
     <div class="content-box">
       <div v-for="(item, index) in titleArray" :key="index">
-        <h2>
-          {{ item }} <span class="badge badge-theme" style="vertical-align: top">{{ webCount[index] }}</span>
+        <h2 :id="item">
+          {{ item }}
+          <span class="badge badge-theme" style="vertical-align: top">{{
+            webCount[index]
+          }}</span>
         </h2>
         <web-links ref="child" :navbars="item" />
       </div>
       <div class="footer">
-        MIT Licensed | Copyright © 2022-present <a href="https://github.com/Okarin1/web-project" target="_blank">@Okarin</a>
+        MIT Licensed | Copyright © 2022-present
+        <a href="https://github.com/Okarin1/web-project" target="_blank"
+          >@Okarin</a
+        >
       </div>
     </div>
+    <side-bar :sideArray="titleArray" @item-click="sideClick" />
   </div>
 </template>
 
 <script>
-import Poem from './Poem.vue'
-import WebLinks from "./WebLinks.vue"
+import Poem from "./Poem.vue";
+import SideBar from "./SideBar.vue";
+import WebLinks from "./WebLinks.vue";
 export default {
-  components: { WebLinks, Poem},
+  components: { WebLinks, Poem, SideBar },
   name: "HelloWorld",
   data() {
     return {
@@ -35,19 +43,23 @@ export default {
         "冷门网站",
       ],
       webCount: [],
-    }
+    };
+  },
+  methods: {
+    sideClick(item) {
+      document.getElementById(item).scrollIntoView();
+    },
   },
   mounted() {
     this.$nextTick(() => {
       for (let i in this.titleArray) {
-        this.webCount.push(this.$refs.child[i].webCount(this.titleArray[i]))
+        this.webCount.push(this.$refs.child[i].webCount(this.titleArray[i]));
       }
-    })
+    });
   },
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <ul class="side-bar">
+      <li
+        v-for="(item, index) in sideArray"
+        :key="item"
+        @click="sideClick(item, index)"
+        :class="{ active: index == currentIndex }"
+      >
+        <p class="side-link">{{ item }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sideArray: {
+      type: Array,
+      default: () => {
+        return []
+      },
+    },
+  },
+  data() {
+    return {
+      currentIndex: 0,
+    }
+  },
+  methods: {
+    sideClick(item, index) {
+      this.currentIndex = index
+      this.$emit("item-click", item)
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.side-bar {
+  width: 12rem;
+  transition: transform 0.25s ease-in-out 0s, opacity 0.25s ease-in-out 0s;
+  transform: translateY(0px);
+  opacity: 1;
+  position: fixed;
+  top: 20rem;
+  bottom: 10rem;
+  right: 2rem;
+  overflow-y: scroll;
+  padding-left: 0;
+  list-style: none;
+  font-size: 12px;
+  li {
+    padding: 0.2rem 0;
+    display: list-item;
+    text-align: -webkit-match-parent;
+    cursor: pointer;
+    border-left: 1px solid @code-color;
+    .side-link {
+      font-size: 1em;
+      font-weight: 400;
+      display: block !important;
+      padding: 0.35rem 1rem 0.35rem 1.25rem;
+      line-height: 1;
+    }
+    p {
+      &:hover {
+        color: @theme-color;
+      }
+    }
+  }
+}
+.side-bar::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+}
+.active {
+  color: @theme-color;
+  border-left-color: @theme-color !important;
+}
+
+@media screen and (max-width: 1024px) {
+  .side-bar {
+    display: none;
+  }
+}
+@media only screen and (max-width: 768px) {
+  .side-bar {
+    display: none;
+  }
+}
+</style>
